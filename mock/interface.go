dd6202bb3a419f87692402c883b4e7eb01ec3702
Eike David Lenz <e.lenz@mittwald.de>
2020-05-18 11:36:27 +0200

    fix goimports not complying to go generate

--- mock/interface.go
+++ mock/interface.go
@@ -5,11 +5,9 @@
 package mockhelmclient
 
 import (
-	reflect "reflect"
-
 	gomock "github.com/golang/mock/gomock"
-	helmclient "github.com/mittwald/go-helm-client"
 	repo "helm.sh/helm/v3/pkg/repo"
+	reflect "reflect"
 )
 
 // MockClient is a mock of Client interface
