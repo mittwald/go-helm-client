d73010a2b63277e3fa434a7f13bc81b8416a67d8
Eike David Lenz <e.lenz@mittwald.de>
2021-06-23 11:46:33 +0200

    return release in InstallOrUpgradeChart method (#29)
    * return release in InstallOrUpgradeChart method
    
    * Update client.go
    
    Co-authored-by: Henning <h.surmeier@mittwald.de>
    
    * return releases too in case of error
    
    Co-authored-by: Henning <h.surmeier@mittwald.de>

--- interface.go
+++ interface.go
@@ -12,7 +12,7 @@ import (
 type Client interface {
 	AddOrUpdateChartRepo(entry repo.Entry) error
 	UpdateChartRepos() error
-	InstallOrUpgradeChart(ctx context.Context, spec *ChartSpec) error
+	InstallOrUpgradeChart(ctx context.Context, spec *ChartSpec) (*release.Release, error)
 	ListDeployedReleases() ([]*release.Release, error)
 	GetRelease(name string) (*release.Release, error)
 	RollbackRelease(spec *ChartSpec, version int) error
