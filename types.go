3427918a51e54374d7999eba9dd20302866a9e70
Steffen <steffen.dangmann@bearingpoint.com>
2022-04-06 01:08:56 +0200

    Introduced "Óutput" in Options for HelmClient configurable io.Writer Default is os.Stdout

--- types.go
+++ types.go
@@ -1,6 +1,7 @@
 package helmclient
 
 import (
+	"io"
 	"time"
 
 	"helm.sh/helm/v3/pkg/getter"
@@ -36,6 +37,7 @@ type Options struct {
 	Debug            bool
 	Linting          bool
 	DebugLog         action.DebugLog
+	Output           io.Writer
 }
 
 // RESTClientGetter defines the values of a helm REST client.
@@ -54,6 +56,7 @@ type HelmClient struct {
 	// ActionConfig is the helm action configuration.
 	ActionConfig *action.Configuration
 	linting      bool
+	output       io.Writer
 	DebugLog     action.DebugLog
 }
 
