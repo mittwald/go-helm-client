17e6bec917e8564090d63aa9e613c5d1155e8066
Nikolai Gut <nikolai.gut@outlook.com>
2023-07-18 16:26:55 +0200

    Revert "OCI Registry has no index file"
    This reverts commit c8722efefd934568e8b233741164f9a2399b0952.

--- types.go
+++ types.go
@@ -13,7 +13,7 @@ import (
 	"helm.sh/helm/v3/pkg/postrender"
 	"helm.sh/helm/v3/pkg/repo"
 
-	"github.com/nikolaigut/go-helm-client/values"
+	"github.com/mittwald/go-helm-client/values"
 )
 
 // Type Guard asserting that HelmClient satisfies the HelmClient interface.
